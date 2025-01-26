15cb88271b2f8829d02187dbc78d6246779d3b2d
hoangnguyenz <nguyenvanhoang2004bn@gmail.com>
2025-01-26 21:35:18 +0700

    video 28

---
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -0,0 +1,33 @@
+package vn.hoidanit.jobhunter.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.hoidanit.jobhunter.domain.User;
+import vn.hoidanit.jobhunter.service.UserService;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/user")
+    public User createUser(@RequestBody User user) {
+        User user2 = this.userService.createUser(user);
+        return user2;
+    }
+
+    @DeleteMapping("/user/{id}")
+    public String createUser(@PathVariable Long id) {
+        this.userService.deleteUserById(id);
+        return "ok !";
+    }
+}
